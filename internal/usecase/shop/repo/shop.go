c08bed0a6e2605c5e249179c56978fd8cab50975
Konstantin Salyaev <r.cf2003@yandex.ru>
2025-02-13 13:38:39 +0300

    refactor(shop): relocate shop to another dir

---
+++ internal/usecase/shop/repo/shop.go
@@ -0,0 +1,59 @@
+package shoprepo
+
+import (
+	_ "embed"
+	"errors"
+	"log"
+	"merch/internal/usecase/shop"
+	"strconv"
+	"strings"
+)
+
+type MapStore struct {
+	store map[string]int
+}
+
+var _ shop.ShopRepo = (*MapStore)(nil)
+
+func (m *MapStore) Buy(userCoin int, name string) (int, error) {
+	v, ok := m.store[name]
+	if !ok {
+		return 0, ErrNoItem
+	}
+	if userCoin < v {
+		return 0, ErrNotEnoughCoin
+	}
+
+	return v, nil
+}
+
+var (
+	ErrNoItem        = errors.New("no such item")
+	ErrNotEnoughCoin = errors.New("no enough coin to buy item")
+)
+
+//go:embed store.txt
+var itemList string
+
+func NewMapStore() *MapStore {
+	items := strings.Split(itemList, "\n")
+
+	result := make(map[string]int, len(items))
+
+	for _, item := range items {
+		fields := strings.Split(item, " ")
+		name, value := fields[0], fields[1]
+		cost, err := strconv.Atoi(value)
+		if err != nil {
+			log.Printf("failed to parse item: %s value: %v", name, value)
+		}
+
+		result[name] = cost
+	}
+
+	return &MapStore{store: result}
+}
+
+func (m *MapStore) QWE() {
+
+}
