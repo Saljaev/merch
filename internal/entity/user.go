f9d99f4e5a4b701076d680f1fb9a233f82ef3bc2
Konstantin Salyaev <r.cf2003@yandex.ru>
2025-02-14 11:31:46 +0300

    feat(entity): add field password to auth

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,10 +1,13 @@
 package entity
 
 import (
+	"fmt"
 	"github.com/sony/sonyflake"
-
+	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 )
+
+const DefaultCoins = 1000
 
 var sf *sonyflake.Sonyflake
 var log = slog.Default()
@@ -18,6 +21,7 @@ func InitSonyflake() {
 
 type User struct {
 	ID        int64
+	Password  string
 	Coins     int
 	UserName  string
 	Inventory []Inventory
@@ -36,18 +40,27 @@ type CoinHistory struct {
 	Type     string
 }
 
-//TODO: user session with token
+func NewUser(username, password string) (User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return User{}, fmt.Errorf("failed to hash password: %w", err)
+	}
 
-func NewUser(coins int, username string) User {
 	userID, err := sf.NextID()
 	if err != nil {
-		log.Error("failed to generate userID", slog.Any("error", err))
+		return User{}, fmt.Errorf("failed to generate userID: %w", err)
 	}
 
 	return User{
 		ID:        int64(userID),
-		Coins:     coins,
+		Coins:     DefaultCoins,
 		UserName:  username,
+		Password:  string(hashedPassword),
 		Inventory: []Inventory{},
-	}
+	}, nil
+}
+
+func (u *User) Identification(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
 }
